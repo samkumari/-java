db78a65b24958115b2e4b96795900066c5a973ba
samkumari <kumarisam65@gmail.com>
2024-06-09 23:23:27 +0530

    Add files via upload

---
+++ Rock_Paper_Scissor.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Rock_Paper_Scissor{
+          
+ public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		Random r=new Random();
+		int x=r.nextInt(3); //random number to be guessed by the user.
+		 System.out.println(x);
+}}
+      
